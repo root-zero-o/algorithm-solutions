41a5e5ec7a94aae04a3c46ea958fa5b5c6c45ed6
영근 <rootzero17@gmail.com>
2022-11-01 22:35:53 +0900

    first commit

---
+++ leetcode/easy/1. Two sum.js
@@ -0,0 +1,26 @@
+//https://leetcode.com/problems/two-sum/
+
+// My answer
+const twoSum = (nums, target) => {
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = 0; j < nums.length; j++) {
+      if (i !== j && nums[i] + nums[j] === target) {
+        return [i, j];
+      }
+    }
+  }
+};
+
+// using ES6 data structure Map
+var twoSum2 = function (nums, target) {
+  let map = new Map();
+
+  for (let i = 0; i < nums.length; i++) {
+    if (map.has(target - nums[i])) {
+      return [map.get(target - nums[i]), i];
+    } else {
+      map.set(nums[i], i);
+    }
+  }
+  return [];
+};
